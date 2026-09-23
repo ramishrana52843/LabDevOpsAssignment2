0460ead48eea40eb4ab43e217f730d34a42eb6d0
ramishrana52843 <ramishrana52843@gmail.com>
2026-09-23 19:58:27 +0500

    Update Code.java

--- Code.java
+++ Code.java
@@ -14,8 +14,9 @@ public class Code {
         Collections.sort(fruits);
         System.out.println(fruits); // [Apple, Banana, Orange]
 
-        System.out.println("Hello User This is Just Demo Code acting as a test for jenkins");
+        System.out.println("Hello User This is Just Demo Code : Test for jenkins");
 
         System.out.println("Name : Ramish Naseer \n  Reg No : SP23-BAI-043");
+        
     }
 }
